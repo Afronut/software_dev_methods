99da7170de8c1416c3b11b5026be1d6cf50574e0
coovi <come8315@colorado.edu>
2019-02-02 13:51:11 -0700

    finish lab 2

--- labs/lab2_git_scripting/lab_2/lab_2_script.sh
+++ labs/lab2_git_scripting/lab_2/lab_2_script.sh
@@ -8,9 +8,15 @@ echo "Enter a pregular Expression"
 read expression
 echo "Enter a file name to search"
 read file
-grep $expression $file
+grep "$expression" $file 
 echo "Your file contain:"
-grep '[0-9]\{3\}-[0-9]\{3\}-' regex_practice.txt | grep -v -c '[0-9]\{3\}/' >
-echo "charaters"
-grep -c '@[a-z].com' regex_practice.txt
-grep $2 $1
+grep '[0-9]\{3\}-[0-9]\{3\}-' regex_practice.txt | grep -v -c '[0-9]\{3\}/'
+echo "phone number."
+echo "Your file contain:"
+grep '.com' regex_practice.txt | grep '@' | grep -c '.com'
+echo "Email."
+
+grep '[0-9]\{3\}-[0-9]\{3\}-' regex_practice.txt | grep -v '[0-9]\{3\}/' | grep '303' >phone_results.txt
+
+grep '@geocities.com' regex_practice.txt > email_results.txt
+grep $2 $1 >command_results.txt
